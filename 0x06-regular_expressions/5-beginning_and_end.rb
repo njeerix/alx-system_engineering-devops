fcaebf3d6d3ac81870585b7dfedd79f793ee0da4
Michelle Njeeri <michellenjeeeri@gmail.com>
2024-01-31 05:01:03 -0800

    update

--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -4,7 +4,7 @@
 input = ARGV[0]
 
 #Define the regular expression to match astring starting with 'h', endingwith 'n', and having any single character in between
-regex = /h.*n/i
+regex = /^hn$/
 
 #Use the regular expression matching method
 match_result = input.match(regex)
