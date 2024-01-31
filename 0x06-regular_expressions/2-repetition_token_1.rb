eccb0b6aabb0d6afdbb1cd03c505c7f8c1eceac0
Michelle Njeeri <michellenjeeeri@gmail.com>
2024-01-31 03:08:31 -0800

    update

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -4,10 +4,10 @@
 input = ARGV[0]
 
 #Define the regular expression to match "hbtn" followed by at least one 't' character
-regex = /hbt+n/
+regex = /hb(t+)n/
 
 #Use the regular expression matching method
 match_result = input.match(regex)
 
 #Print the match result or an empty string if no match
-puts match_result ? match_result[0]: " "
+puts match_result ? match_result[0] : ""
