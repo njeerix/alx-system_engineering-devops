35e7cf2a4053b20b248c0647d1e7d8b230ab8a0d
Michelle Njeeri <michellenjeeeri@gmail.com>
2024-01-31 05:48:21 -0800

    update

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,13 +1,13 @@
 #!/usr/bin/env ruby
 
 #Get the log file path from the command line
-log_file_path = ARGV[0]
-
-#Read the log file content
-log_content = File.read(log_file_path)
+input = ARGV[0]
 
 #Define a regular expression to extract relevant information
-regex = /\[from:(.*?)\]\[to:(.*?)\]\[flags:(.*?)\]/
+regex = /^\S+ \d+ \d+:\d+:\d+ ip-[\d.]+ mdr: \d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} Sent SMS .* from:([^\]]+) \[to:([^\]]+) \[flags:([^\]]+)/
 
 #Use the regular expression to find matches in the log content
-matches = log_content.scan(regrex)
+match_result = input.match(regrex)
+
+#Print the match result or an empty string if no match
+puts match_result ? "#{match_result[1]},#{match_result[2]},#{match_result[3]}" : ""
