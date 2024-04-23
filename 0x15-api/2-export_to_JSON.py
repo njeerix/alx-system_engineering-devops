3246d0fba2c689678280fcdcb03236dfab9da2f1
Michelle Njeeri <michellenjeeeri@gmail.com>
2024-04-23 07:00:54 -0700

    api

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -4,6 +4,7 @@ import sys
 import json
 import requests
 
+
 def fetch_employee_todo_progress(employee_id):
     base_url = 'https://jsonplaceholder.typicode.com'
     user_url = f'{base_url}/users/{employee_id}'
