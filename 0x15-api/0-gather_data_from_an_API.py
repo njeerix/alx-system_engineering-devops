ab7c4b7f00954cc36898c3dac57c71fa2d131575
Michelle Njeeri <michellenjeeeri@gmail.com>
2024-04-23 06:05:17 -0700

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,6 +2,7 @@
 import sys
 import requests
 
+"""Exports to-do list information of all employees to JSON format."""
 def fetch_employee_todo_progress(employee_id):
     base_url = 'https://jsonplaceholder.typicode.com'
     user_url = f'{base_url}/users/{employee_id}'
