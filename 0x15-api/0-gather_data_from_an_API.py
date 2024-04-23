e0ea0320af485ffa1fa2f15f4a7e50d18fb31a88
Michelle Njeeri <michellenjeeeri@gmail.com>
2024-04-23 06:15:56 -0700

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -25,7 +25,7 @@ def fetch_employee_todo_progress(employee_id):
         total_tasks = len(todos_data)
         completed_tasks = sum(1 for todo in todos_data if todo['completed'])
         # Print progress information
-        print(f"{employee_name} finished {completed_tasks}/{total_tasks} tasks:")
+        print(employee_name + f" completed {completed_tasks}/{total_tasks} tasks:")
         for todo in todos_data:
             if todo['completed']:
                 print(f"    {todo['title']}")
